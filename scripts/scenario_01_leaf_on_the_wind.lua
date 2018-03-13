76bfc04623192de98ea724b82407a808c1bc20fd
Yarek Tyshchenko <yarek.tyshchenko@awin.com>
2018-03-13 11:11:36 +0000

    Add scenario

---
+++ scripts/scenario_01_leaf_on_the_wind.lua
@@ -0,0 +1,12 @@
+-- Name: Leaf on the wind
+-- Description: Scenario Description
+-- Type: Basic
+-- Variation[OnePlayer]: One player scenario
+-- Variation[TwoPlayer]: Two player scenario
+
+function init()
+end
+
+function update(delta)
+    --No victory condition
+end
