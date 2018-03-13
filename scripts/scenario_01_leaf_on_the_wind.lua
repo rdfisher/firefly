03c342d24319b0d33d1d704c8942afc8ed8d1e81
Yarek Tyshchenko <yarek.tyshchenko@awin.com>
2018-03-13 16:35:13 +0000

    Attempt to implement find probes function

--- scripts/scenario_01_leaf_on_the_wind.lua
+++ scripts/scenario_01_leaf_on_the_wind.lua
@@ -3,11 +3,46 @@
 -- Type: Basic
 -- Variation[TwoPlayer]: Two player scenario
 
+-- TODO:
+-- Last position
+-- disperse stations
+-- Create merchant fleet of transports
+-- Spawn mission types (transport, salvage, data hack)
+-- Create Allience Navy fleet
+-- Create Reever incursion script
+-- Add Ships (Tohoku-class cruiser, Allience Navy Gunships, Firefly, Reever ships?)
+-- Create a "follow investigate last position" script
+-- Create a search pattern script
+-- Implement Cry Baby
+
 function init()
+    browncoat = PlayerSpaceship():setFaction("Browncoats"):setTemplate("Atlantis")
     if getScenarioVariation() == "TwoPlayer" then
     end
+
+    -- Temporary function to test finding probes
+    addGMFunction("Find Probes", function()
+        probes = findProbes()
+        for _,v in ipairs(probes) do
+            if v:isValid() then
+                local x, y = v:getPosition()
+                print(string.format("Probe location: %f, %f", x, y))
+            end
+        end
+    end)
 end
 
 function update(delta)
-    --No victory condition
+end
+
+function findProbes()
+    list = {}
+    for _, probe in ipairs(getAllObjects()) do
+        if probe.typeName == "ScanProbe" then
+            if probe:getPosition() == probe:getTarget() then
+                table.insert(list, probe)
+            end
+        end
+    end
+    return list
 end
