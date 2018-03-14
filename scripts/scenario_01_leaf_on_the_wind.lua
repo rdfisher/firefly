a10e13c09436f0c898063f19bdc8e648c8048474
Yarek Tyshchenko <yarek.tyshchenko@awin.com>
2018-03-14 12:09:51 +0000

    Spawn a bunch of civvies, and get Alliance Navy following them

--- scripts/scenario_01_leaf_on_the_wind.lua
+++ scripts/scenario_01_leaf_on_the_wind.lua
@@ -14,10 +14,26 @@
 -- Create a "follow investigate last position" script
 -- Create a search pattern script
 -- Implement Cry Baby
-
+require("CivilianGroup.lua")
+groups = {}
 function init()
     browncoat = PlayerSpaceship():setFaction("Browncoats"):setTemplate("Atlantis")
-    if getScenarioVariation() == "TwoPlayer" then
+    
+    -- Spawn some stations
+    SpaceStation():setTemplate("Medium Station"):setFaction("Independent"):setPosition(20000, 20000):setCallSign("DS1")
+    SpaceStation():setTemplate("Medium Station"):setFaction("Independent"):setPosition(-20000, 20000):setCallSign("DS2")
+    SpaceStation():setTemplate("Medium Station"):setFaction("Independent"):setPosition(-20000, -20000):setCallSign("DS3")
+    SpaceStation():setTemplate("Medium Station"):setFaction("Independent"):setPosition(20000, -20000):setCallSign("DS4")
+
+    for i=1,4 do
+        for c=1,10 do
+            ship = CpuShip():setTemplate("Flavia"):setFaction("Independent"):setPosition(random(-10000, 10000), random(-10000, 10000)):orderRoaming()
+            group = CivilianGroup:new()
+            group:add(ship)
+        end
+        local x, y = group:getGeographicCentre()
+        escort = CpuShip():setTemplate("Starhammer II"):setFaction("Allience Navy"):setPosition(x,y):orderRoaming()
+        table.insert(groups, {group=group, escort=escort})
     end
 
     -- Temporary function to test finding probes
@@ -33,6 +49,11 @@ function init()
 end
 
 function update(delta)
+    -- todo: make escort follow the center
+    for _, group in ipairs(groups) do
+        local x, y = group.group:getGeographicCentre()
+        group.escort:orderFlyTowards(x, y)
+    end
 end
 
 function findProbes()
