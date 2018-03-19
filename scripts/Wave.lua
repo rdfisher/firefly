d552eb0961b24ede935be92d894ef07443609dd3
Yarek Tyshchenko <yarek.tyshchenko@awin.com>
2018-03-19 13:00:10 +0000

    Fix apb

--- scripts/Wave.lua
+++ scripts/Wave.lua
@@ -1,12 +1,12 @@
 Wave = {
   sender = {},
   recipients = {},
-  messages = {},
-  last_message = nil
+  messages = {}
 }
 
 function Wave:new(sender)
   local o = {}
+  o.last_message = ""
   o.sender = sender
   setmetatable(o, self)
   self.__index = self
@@ -17,31 +17,42 @@ function Wave:registerListener(ship)
   table.insert(self.recipients, ship)
 end
 
-function Wave:sameAsLastMessage(message)
-  if self.last_message ~= nil then
-    if self.last_message.callsign ~= message.callsign and self.last_message.sector ~= message.sector then
-      self.last_message = message
-      return false
-    end
+function Wave:stupidLua(a, b)
+  if string.len(a) ~= string.len(b) then
+    return false
   end
 
-  self.last_message = message
+  if string.find(a, b) == nil then
+    return false
+  end
   return true
+end
+
+function Wave:sameAsLastMessage(message)
+  -- return false
+  if self:stupidLua(self.last_message, message) then
+    return true
+  end
+  self.last_message = message
+  return false
 end
 
 function Wave:message(message)
   -- Lets not spam players with messages
-  if self:sameAsLastMessage(message) then
+  if self.messages[message] == true then
     return
   end
-  table.insert(self.messages, message)
+  self.messages[message] = true
   for i, recipient in ipairs(self.recipients) do
     self.sender:sendCommsMessage(recipient, message)
   end
 end
 
 function Wave:getAccumulatedMessages()
-  local messages = self.messages
+  local messages = {}
+  for message, _ in ipairs(self.messages) do
+    table.insert(output, message)
+  end
   self.messages = {}
   return messages
 end
